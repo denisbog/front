df9a04515d15eebe57314605239f4ccd42e5c0fa
Denis <denis.boghiu@gmail.com>
2023-10-11 21:40:46 +0300

    bg image

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Post from "./components/Post";
 function App() {
   const items = 8
   return (
-    <div class="h-screen w-screen">
-      <img class="absolute" src="http://localhost:3000/background.jpg" />
+    <div class="h-screen w-screen bg-[url('background.jpg')] bg-cover">
       <div class="absolute p-3 h-14 w-screen bg-white bg-opacity-20 text-white text-3xl items-center">Home page</div>
       <div class="relative p-3 w-screen gap-3 top-16 flex overflow-x-scroll">
         {
