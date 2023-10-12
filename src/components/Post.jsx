bc8643bbc394f1315fb6ff79c87c5ed0f83d0cb1
Denis <denis.boghiu@gmail.com>
2023-10-12 21:50:27 +0300

    adding animation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,8 @@
 export default function Post() {
   return (
-    <div class="w-48 h-96 bg-white flex-shrink-0">Post</div>
+    <div class="transition-all duration-200 hover:m-1 hover:translate-y-2 w-48 h-96 rounded bg-white flex-shrink-0 p-3 font-light">
+      <div class="font-bold text-center pt-14">Post</div>
+      <div class="pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+    </div>
   )
 }
