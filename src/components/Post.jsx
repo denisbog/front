f1814cce20d11c037dc0c1c511eae3962597bac7
Denis <denis.boghiu@gmail.com>
2023-10-13 20:28:01 +0300

    adding button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,10 @@
 export default function Post() {
   return (
-    <div class="transition-all duration-200 hover:m-1 hover:translate-y-2 w-48 h-96 rounded bg-white flex-shrink-0 p-3 font-light">
+    <div class="transition-all ease-in duration-150 hover:m-1 hover:translate-y-2 w-48 h-96 rounded bg-white flex-shrink-0 p-3 font-light hover:shadow-lg select-none relative">
       <div class="font-bold text-center pt-14">Post</div>
       <div class="pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+
+      <div class="absolute bottom-0 inset-x-0 p-3 flex flex-row-reverse"><botton class="rounded duration-150 hover:shadow p-1 ring-1 ring-blue-50 hover:ring-blue-300 ease-in">Details</botton></div>
     </div>
   )
 }
