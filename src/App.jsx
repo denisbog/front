2c9fd4466ee091ad2ce0fcfd70dd2cdde1cba139
Denis <denis.boghiu@gmail.com>
2023-10-18 22:49:34 +0300

    click to display modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+import { createSignal } from "solid-js";
 import Map from "./components/Map";
 import Post from "./components/Post";
 
 function App() {
+  const [warning, setWarning] = createSignal(false)
   const items = 8
+
+
   return (
     <div class="h-min-screen w-screen bg-[url('background.jpg')] bg-cover relative">
+      {
+        warning() &&
+        < div class="fixed z-[2000] bg-gray-500 bg-opacity-75 inset-0 flex justify-center items-center">
+          <div class="bg-white p-2 w-96 h-96 shadow-2xl rounded" onclick={() => setWarning(false)}>modal</div>
+        </div>
+      }
       <div class="p-3 h-14 w-screen bg-white bg-opacity-20 text-white text-3xl items-center">Home page</div>
       <div class="p-3 w-screen gap-3 top-16 flex overflow-x-scroll">
         {
           Array.from({ length: items }).map(_element => {
-            return <Post />
+            return <Post act={() => setWarning(!warning())} />
           })
         }
       </div>
-
       <div class="flex h-[75vh]">
         <div class="w-96 overflow-auto">
           {
@@ -26,7 +35,7 @@ function App() {
       </div>
       <div class="h-48"></div>
       <div class="absolute bottom-0 inset-x-0 bg-white h-36 bg-opacity-20 text-white p-5">footer</div>
-    </div>
+    </div >
   )
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,11 @@
-export default function Post() {
+export default function Post(props) {
   return (
     <div class="transition-all ease-in duration-150 hover:mx-2 hover:bg-gradient-to-tr from-pink-200 to-blue-300 w-48 h-96 rounded bg-white flex-none p-3 font-light hover:shadow-lg select-none relative">
       <div class="font-bold text-center pt-14">Post</div>
       <div class="pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
-
-      <div class="absolute bottom-0 inset-x-0 p-3 flex flex-row-reverse"><botton class="rounded duration-150 hover:shadow p-1 ring-1 ring-blue-50 hover:ring-blue-300 ease-in">Details</botton></div>
+      <div class="absolute bottom-0 inset-x-0 p-3 flex flex-row-reverse">
+        <botton class="rounded duration-150 hover:shadow p-1 ring-1 ring-blue-50 hover:ring-blue-300 ease-in" onclick={props.act}>Details</botton>
+      </div>
     </div>
   )
 }
