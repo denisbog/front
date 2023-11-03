12819872639645e9abd65e13d34023ae61d3900a
Denis <denis.boghiu@gmail.com>
2023-11-03 22:18:56 +0200

    initial translation

--- src/components/Fragment.jsx
+++ src/components/Fragment.jsx
@@ -6,14 +6,15 @@ export default function Fragment(props) {
 
   return <>
     <Motion.div
-      class="rounded bg-white bg-opacity-70 p-2 m-2" onClick={() => toogle(!show())}>{props.item.original}</Motion.div>
+      class="rounded bg-white bg-opacity-70 p-2 m-2 select-none" onClick={() => toogle(!show())}>{props.item.original}</Motion.div>
     <Presence>
       <Show when={show()}>
         <Motion.div
-          animate={{ opacity: [0, 1], scaleY: [0.6, 1], translate: 0 }}
+          initial={{ translate: 200 }}
+          animate={{ opacity: [0, 1], scale: [0.6, 1], translate: 0 }}
           transition={{ duration: 0.2, easing: "ease-in-out" }}
-          exit={{ opacity: [1, 0], scaleY: [1, 0.6], translate: 200 }}
-          class="rounded bg-green-300 bg-opacity-70 p-2 m-2">{props.item.translation}</Motion.div>
+          exit={{ opacity: [1, 0], scale: [1, 0.6], translate: 200 }}
+          class="rounded bg-green-300 bg-opacity-70 p-2 m-2 select-none" onClick={() => toogle(false)}>{props.item.translation}</Motion.div>
       </Show>
     </Presence>
   </>
